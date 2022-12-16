637529f566ed486e77bcaf3041658ebe6ad672ee
agievaliliyaa <agievaliliyaa@gmail.com>
2022-12-16 22:19:24 +0300

    Add project files.

---
+++ 16.12.22/Rational.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project11
+{
+    [DeveloperInfo("Adell", Date = "26.05.2004")]
+
+    class Rational
+    {
+        public int numerator;
+        public int denumerator;
+    }
+
+}
+
